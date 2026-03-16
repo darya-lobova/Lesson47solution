14b871f99d67ff84e54bffb12158b3e00c73bcc0
st <st@A510PC3>
2026-03-16 17:34:13 +0300

    Task02 solved

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -5,28 +5,38 @@ bool is_the_perfect_number(long long number) {
 	if (number % 2 != 0) {
 		return false;
 	}
+	//
+	//	int sum = 1;
+	//	int n = number / 2;
+	//	for (int div = 2; div <= n; div++) {
+	//		if (number % div == 0) {
+	//			sum += div;
+	//		}
+	//	}
+	//
+	//	return sum == number;
+	//}
+	int size = 7;
+	int perfect_array[]{ 6,28,496,8128,33550336,8589869056,137438691328 };
 
-	int sum = 1;
-	int n = number / 2;
-	for (int div = 2; div <= n; div++) {
-		if (number % div == 0) {
-			sum += div;
+	for (int i = 0; i < size; i++) {
+		if (number == perfect_array[i]) {
+			return true;
 		}
 	}
-
-	return sum == number;
+	return false;
 }
 
-	/*switch (number) {
-	case 6:
-	case 28:
-	case 496:
-	case 8128:
-	case 33550336:
-	case 8589869056:
-	case 137438691328:
-		return true;
-	default:
-		return false;
-	}
+/*switch (number) {
+case 6:
+case 28:
+case 496:
+case 8128:
+case 33550336:
+case 8589869056:
+case 137438691328:
+	return true;
+default:
+	return false;
+}
 }*/
