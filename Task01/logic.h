708d5271ae66f0d0a2174f1f74740ee75357f2c3
st <st@A510PC3>
2026-03-16 17:12:43 +0300

    Task01 solved

--- Task01/logic.h
+++ Task01/logic.h
@@ -1,3 +1,6 @@
 #include <iostream>
 
 using namespace std;
+
+int get_min_digit(int number);
+int get_max_digit(int number);
--- Task01/main.cpp
+++ Task01/main.cpp
@@ -2,5 +2,13 @@
 
 int main() {
 
+	int number;
+
+	cout << "Input number: ";
+	cin >> number;
+
+	cout << "Min digit: " << get_min_digit(number) << endl;
+	cout << "Max digit: " << get_max_digit(number) << endl;
+
 	return 0;
 }
